ca106108e423f3bbbaf8e0d2bc2fd06d2be77a59
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-12-14 16:32:14 -1000

    fix: support previewing non-page documents

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,11 +4,19 @@ import { Helmet } from 'react-helmet-async'
 import { withUnpublishedPreview } from 'gatsby-source-prismic'
 import MapSlicesToComponents from '@walltowall/react-map-slices-to-components'
 
-import { Layout } from '../components/Layout'
+import { NotFoundPageQuery } from '../types.generated'
 import { slicesMap } from '../slices/PageBody'
-import { mapDataToPropsEnhancer, PageTemplate } from '../templates/page'
+import {
+  mapDataToPropsEnhancer,
+  PageTemplate,
+  slicesMiddleware,
+} from '../templates/page'
 import { useSiteSettings } from '../hooks/useSiteSettings'
+import { NewsPostTemplate } from '../templates/news_post'
+import { ProjectTemplate } from '../templates/project'
 import { InteriorPageTemplate } from '../templates/interior_page'
+
+import { Layout } from '../components/Layout'
 
 /**
  * Mapping of Prismic custom type API IDs to their templates. Add mappings here
@@ -19,15 +27,16 @@ import { InteriorPageTemplate } from '../templates/interior_page'
 const customTypeToTemplate = {
   page: PageTemplate,
   interior_page: InteriorPageTemplate,
+  news_post: NewsPostTemplate,
+  project: ProjectTemplate,
 }
 
-const sliceList = [
-  { __typename: 'PageBodyHeader', id: 'header' },
-  { __typename: 'PageBodyFooter', id: 'footer' },
-]
-
-export const NotFoundPage = ({ data, location }: PageProps) => {
+export const NotFoundPage = ({
+  data,
+  location,
+}: PageProps<NotFoundPageQuery>) => {
   const siteSettings = useSiteSettings()
+  const page = data?.prismicPage
 
   /**
    * Metadata made available in a slice's `mapDataToProps` and
@@ -46,12 +55,21 @@ export const NotFoundPage = ({ data, location }: PageProps) => {
   return (
     <Layout>
       <Helmet>
-        <title>Page not found | {siteSettings.siteName}</title>
+        <title>
+          {page?.data?.meta_title ??
+            page?.data?.title?.text ??
+            'Page not found'}{' '}
+          | {siteSettings.siteName}
+        </title>
+        {page?.data?.meta_description && (
+          <meta name="description" content={page?.data?.meta_description} />
+        )}
       </Helmet>
       <MapSlicesToComponents
-        list={sliceList}
+        list={page?.data?.body}
         map={slicesMap}
         meta={meta}
+        listMiddleware={slicesMiddleware}
         mapDataToPropsEnhancer={mapDataToPropsEnhancer}
       />
     </Layout>
