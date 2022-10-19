d176e5746054899effc9e5c86533df2bd0019872
Anthony Yarbrough <asyarb@hawaii.edu>
2022-10-19 11:33:02 -1000

    fix: dont scroll for cohort tab updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,11 @@
 import './src/global'
 
 export { wrapRootElement } from './src/global'
+
+export const shouldUpdateScroll = ({ routerProps, prevRouterProps }) => {
+	const isCohortTabUpdate =
+		routerProps.location.search?.includes('cohort') ||
+		prevRouterProps.location.search?.includes('cohort')
+
+	return !isCohortTabUpdate
+}
